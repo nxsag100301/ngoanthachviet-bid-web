94350db304a808060804474d01ff046f2224a686
nxsag100301 <nguyenxuansang10032001@gmail.com>
2025-10-23 11:35:12 +0700

    fix-bottom-mobile

--- src/pages/DetailAuctionProduct/components/GeneralProductInformation.jsx
+++ src/pages/DetailAuctionProduct/components/GeneralProductInformation.jsx
@@ -31,36 +31,34 @@ const GeneralProductInformation = () => {
         <CountdownTimer type={2} size='large' />
         <div className='hidden sm:block px-2 py-1 sm:px-6 sm:py-4 md:px-4 md:py-2 lg:px-6 lg:py-4 rounded-[12px] border border-gray-100 bg-white space-y-6'>
           <div className='flex flex-col gap-2 sm:gap-3'>
-            <p className='text-[14px] sm:text-[18px] md:text-[14px] lg:text-[18px] leading-[26px] text-gray-950'>
-              Chưa tham gia
-            </p>
+            <p className='text-[14px] leading-5 text-gray-950'>Chưa tham gia</p>
             <div className='flex flex-row justify-between'>
-              <p className='text-[14px] sm:text-[18px] md:text-[14px] lg:text-[18px] leading-[26px] text-text-500'>
+              <p className='text-[14px] leading-5 text-text-500'>
                 Giá hiện tại
               </p>
-              <p className='text-[14px] sm:text-[18px] md:text-[14px] lg:text-[18px] leading-[26px] text-text-950 font-semibold'>
+              <p className='text-[14px] leading-5 text-text-950 font-semibold'>
                 10.000.000 VND
               </p>
             </div>
             <div className='flex flex-row justify-between'>
               <Chip label={'Tự động đặt giá'} type='error' />
-              <p className='text-[14px] sm:text-[18px] md:text-[14px] lg:text-[18px] leading-[26px] text-success-600 font-semibold'>
+              <p className='text-[14px] leading-5 text-success-600 font-semibold'>
                 Đang dẫn đầu
               </p>
             </div>
             <div className='flex flex-row justify-between'>
-              <p className='text-[14px] sm:text-[18px] md:text-[14px] lg:text-[18px] leading-[26px] text-text-500'>
+              <p className='text-[14px] leading-5 text-text-500'>
                 Tự tăng mỗi lần
               </p>
-              <p className='text-[14px] sm:text-[18px] md:text-[14px] lg:text-[18px] leading-[26px] text-text-950 font-semibold'>
+              <p className='text-[14px] leading-5 text-text-950 font-semibold'>
                 3.000.000 VND
               </p>
             </div>
             <div className='flex flex-row justify-between'>
-              <p className='text-[14px] sm:text-[18px] md:text-[14px] lg:text-[18px] leading-[26px] text-text-500'>
+              <p className='text-[14px] leading-5 text-text-500'>
                 Giá tối đa của bạn
               </p>
-              <p className='text-[14px] sm:text-[18px] md:text-[14px] lg:text-[18px] leading-[26px] text-text-950 font-semibold'>
+              <p className='text-[14px] leading-5 text-text-950 font-semibold'>
                 35.000.000 VND
               </p>
             </div>
@@ -74,44 +72,42 @@ const GeneralProductInformation = () => {
       {/* Bottom mobile */}
       <div
         className='fixed sm:hidden bottom-0 bg-white w-full
-      rounded-t-[24px] -mx-4 shadow-[0_-2px_8px_rgba(0,0,0,0.2)] p-4 border border-gray-100 z-20'
+      rounded-t-[24px] -mx-4 shadow-[0_-3px_10px_rgba(0,0,0,0.1)] p-4 border border-gray-100 z-20'
       >
         <div className='h-1 w-11 bg-gray-100 rouned-full mx-auto' />
-        <div className='flex flex-col gap-2 sm:gap-3'>
-          <p className='text-[14px] sm:text-[18px] md:text-[14px] lg:text-[18px] leading-[26px] text-gray-950 font-semibold'>
+        <div className='flex flex-col gap-[10px]'>
+          <p className='text-[14px] leading-5 text-gray-950 font-semibold'>
             Chưa tham gia
           </p>
           <div className='flex flex-row justify-between'>
-            <p className='text-[14px] sm:text-[18px] md:text-[14px] lg:text-[18px] leading-[26px] text-text-500'>
-              Giá hiện tại
-            </p>
-            <p className='text-[14px] sm:text-[18px] md:text-[14px] lg:text-[18px] leading-[26px] text-text-950 font-semibold'>
+            <p className='text-[14px] leading-5 text-text-500'>Giá hiện tại</p>
+            <p className='text-[14px] leading-5 text-text-950 font-semibold'>
               10.000.000 VND
             </p>
           </div>
           <div className='flex flex-row justify-between'>
             <Chip label={'Tự động đặt giá'} type='error' />
-            <p className='text-[14px] sm:text-[18px] md:text-[14px] lg:text-[18px] leading-[26px] text-success-600 font-semibold'>
+            <p className='text-[14px] leading-5 text-success-600 font-semibold'>
               Đang dẫn đầu
             </p>
           </div>
           <div className='flex flex-row justify-between'>
-            <p className='text-[14px] sm:text-[18px] md:text-[14px] lg:text-[18px] leading-[26px] text-text-500'>
+            <p className='text-[14px] leading-5 text-text-500'>
               Tự tăng mỗi lần
             </p>
-            <p className='text-[14px] sm:text-[18px] md:text-[14px] lg:text-[18px] leading-[26px] text-text-950 font-semibold'>
+            <p className='text-[14px] leading-5 text-text-950 font-semibold'>
               3.000.000 VND
             </p>
           </div>
           <div className='flex flex-row justify-between'>
-            <p className='text-[14px] sm:text-[18px] md:text-[14px] lg:text-[18px] leading-[26px] text-text-500'>
+            <p className='text-[14px] leading-5 text-text-500'>
               Giá tối đa của bạn
             </p>
-            <p className='text-[14px] sm:text-[18px] md:text-[14px] lg:text-[18px] leading-[26px] text-text-950 font-semibold'>
+            <p className='text-[14px] leading-5 text-text-950 font-semibold'>
               35.000.000 VND
             </p>
           </div>
-          <Button className='w-full h-[50px] mt-2 px-10 py-6 lg:rounded-[30px] ml-auto lg:text-[20px]'>
+          <Button className='w-full mt-1 px-10 py-6 lg:rounded-[30px] ml-auto lg:text-[20px]'>
             Ra giá
           </Button>
         </div>
