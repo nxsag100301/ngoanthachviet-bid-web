81164a68878056c99a02b89d52a38715f049b2fc
nxsag100301 <nguyenxuansang10032001@gmail.com>
2025-10-24 10:35:49 +0700

    allow-auth

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from 'react-router-dom'
 
 const PrivateRoutes = () => {
   const { userInfo } = useSelector((state) => state.user)
-  const isAuthenticated = !!userInfo
+  const isAuthenticated = true
   return <>{isAuthenticated ? <Outlet /> : <Navigate to='/login' replace />}</>
 }
 
